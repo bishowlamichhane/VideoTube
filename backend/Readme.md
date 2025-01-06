b0b6595fffe19ae8338d1d21fb1b12f47eba8c50
bishowlamichhane <lamichhanebisho4545@gmail.com>
2025-01-06 13:07:28 +0545

    first

---
+++ backend/Readme.md
@@ -0,0 +1,5 @@
+Aggregation-Pipelines
+
+This demonstration shows how to use MongoDB aggregation pipelines in a video streaming app to manage user subscriptions, retrieve subscriber counts, and channels a user is subscribed to. Includes code examples using $match, $lookup, $addFields, and $project.
+
+Also Shows how to retrieve watch History of a User
---
+++ frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.css";
+import App from "./routes/App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
